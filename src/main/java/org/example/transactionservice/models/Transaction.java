e5ad6cc36fc730cfae4e73d80257aed4b7ccd2a8
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-04-20 13:02:08 +0530

    model: added Transaction model

---
+++ src/main/java/org/example/transactionservice/models/Transaction.java
@@ -0,0 +1,58 @@
+package org.example.transactionservice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Transaction extends  BaseModel {
+
+    @Column(unique = true, nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private String senderId;
+
+    @Column(nullable = false)
+    private String receiverId;
+
+    private String merchantId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
+
+    private String description;
+
+    @Column(unique = true, nullable = false)
+    private String idempotencyKey;
+
+    private String paymentGateway;
+
+    private String gatewayTransactionId;
+
+    // for split payments
+    private Boolean isSplitPayment;
+    private String parentTransactionId;
+
+    @PrePersist
+    public void prePersist() {
+        if (transactionId == null) {
+            transactionId = UUID.randomUUID().toString();
+        }
+    }
+}
