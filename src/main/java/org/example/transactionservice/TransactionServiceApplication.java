318af71381118aa2d195fbd5fd540e1f737df525
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-04-20 11:14:15 +0530

    init: spring boob project init

---
+++ src/main/java/org/example/transactionservice/TransactionServiceApplication.java
@@ -0,0 +1,14 @@
+package org.example.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TransactionServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+}
